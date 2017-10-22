6f5ae970bfcca74d804b0e4d4261171f8da489d8
Matthew Fung <matthewfung@Matthews-MacBook-Pro.local>
2017-10-22 13:25:39 -0400

    Added functionality to category-list page

--- amg-reader/src/pages/category-list/category-list.ts
+++ amg-reader/src/pages/category-list/category-list.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { HttpProvider, Requests } from '../../providers/http/http';
 /**
  * Generated class for the CategoryListPage page.
  *
@@ -14,12 +14,32 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'category-list.html',
 })
 export class CategoryListPage {
+  categories:Array<JSON>;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public httpProvider: HttpProvider) {
+    
   }
 
+ /*  */
+  printCategories(categories){
+    categories.forEach(element => {
+      console.log(element.title)
+    });
+  };
+  
+// If development = true
   ionViewDidLoad() {
-    console.log('ionViewDidLoad CategoryListPage');
+    this.httpProvider.get(Requests.categories).then(value=>{this.printCategories(value.data.results);
+    this.categories=value.data.results});
+    
   }
 
-}
+  // // If development = false
+  // ionViewDidLoad() {
+  //   this.httpProvider.get(Requests.categories).then(value=>{;
+  //   this.categories=value.results});
+    
+  // }
+
+
+  }
