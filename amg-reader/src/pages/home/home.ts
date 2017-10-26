28dc1f0129505252c35677bef7cdc19b6976ff0a
marktdodds <marktdodds@gmail.com>
2017-10-26 15:07:08 -0400

    updated styling of list view of blogs

--- amg-reader/src/pages/home/home.ts
+++ amg-reader/src/pages/home/home.ts
@@ -1,6 +1,5 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
-import {HttpProvider, Requests} from '../../providers/http/http';
 import {BlogListPage} from '../blog-list/blog-list';
 
 @Component({
